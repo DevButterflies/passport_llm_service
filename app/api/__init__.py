51d6ffb4162e94e82b11b3eefe877209b36e8e83
Rabii Nasri <rabii.nasri1@gmail.com>
2026-04-27 15:43:35 +0100

    remove the hardcoded API keys and replace them with the new ones. Also, ensure that the .env file is correctly loaded in the __init__.py file.

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,22 +1,38 @@
-from app.core.llm_client import ThreadManager
-from dotenv import load_dotenv
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+from app.core.llm_client import ThreadManager
 
-parent_dir = Path(__file__).resolve().parent.parent  
+# 1. Get the current file path (root/app/api/__init__.py)
+current_file = Path(__file__).resolve()
 
-env_path = parent_dir / "api_keys.env"
-MODEL_PATH = parent_dir / "assets" / "best.onnx"
-print(f"Loading environment variables from: {env_path}")
+project_root = current_file.parent.parent.parent
+
+# 3. Define the path to your .env file
+env_path = project_root / "api_keys.env"
+
+
+print(f"Looking for .env at: {env_path}")
+if not env_path.exists():
+    print("❌ ERROR: api_keys.env NOT found at this path!")
+else:
+    print("✅ Found api_keys.env, loading now...")
+
+# 4. Load the environment variables
 load_dotenv(dotenv_path=env_path)
-#gemini-2.5-flash-lite
+
+# Update MODEL_PATH as well (assuming assets is in root/assets)
+MODEL_PATH = project_root / "assets" / "best.onnx"
+
+# CONFIGS
 CONFIGS_MAIN = [
-    {"name": f"gemini-worker-{i}", "model": "gemini-3.1-flash-lite-preview", "api_key": "AIzaSyCLHTFUNi6O1MMxn6n3jYs1JHbqVvdaOcA"}
+    {"name": f"gemini-worker-{i}", "model": "gemini-3.1-flash-lite-preview", "api_key": os.getenv(f"GOOGLE_API_KEY_{i}") }
     for i in range(1, 5)
 ]
 
 CONFIGS_LIGHT = [
-    {"name": f"gemini-worker-{i}", "model": "gemini-2.5-flash-lite", "api_key": "AIzaSyCLHTFUNi6O1MMxn6n3jYs1JHbqVvdaOcA"}
+    {"name": f"gemini-worker-{i}", "model": "gemini-2.5-flash-lite", "api_key": os.getenv(f"GOOGLE_API_KEY_{i}") }
     for i in range(1, 5)
 ]
 
-llm = ThreadManager(model1_configs=CONFIGS_MAIN , model2_configs =CONFIGS_LIGHT )
+llm = ThreadManager(model1_configs=CONFIGS_MAIN, model2_configs=CONFIGS_LIGHT)
